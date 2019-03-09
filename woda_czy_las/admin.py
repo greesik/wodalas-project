f820f6a237af1e6d5d53362f5422c3b8a45eef0d
Grzegorz Sikora <greesik@gmail.com>
2019-03-09 20:21:34 +0100

    models and database created

--- woda_czy_las/admin.py
+++ woda_czy_las/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import WodaInfo, LasInfo
 # Register your models here.
+admin.site.register(WodaInfo)
+admin.site.register(LasInfo)
--- woda_czy_las/models.py
+++ woda_czy_las/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class WodaInfo(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
+
+class LasInfo(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
